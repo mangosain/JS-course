4a1ea27a5f5262ea16c2d867ca3a175755d4aed0
Manav Gosain <k.gosain7140@gmail.com>
2025-12-12 01:26:30 +0530

    added object destructuring

---
+++ Course 1/Module 1/object_destructuring.js
@@ -0,0 +1,11 @@
+const obj = {
+  title: "Top Gun",
+  year: "1986",
+  genre: "action",
+  star: "Tom Cruise",
+  director: "Tony Scott",
+};
+
+const { title, director } = obj;
+
+console.log(`Movie: ${title}\nDirected by: ${director}`);
